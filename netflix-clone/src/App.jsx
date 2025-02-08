7ec3a213af22688fb20a8f3389204828f28d65b0
Dereje Masresha <dbazmm3@gmail.com>
2025-02-09 00:43:17 +0300

    header and footer done

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './page/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>Hello</h1>
+     <h1>
+      <Home />
+     </h1>
     </>
   )
 }
