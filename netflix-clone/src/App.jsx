d63c5758689f4091643fb1df83d0d8715ed27fa7
Dereje Masresha <dbazmm3@gmail.com>
2025-02-09 13:49:35 +0300

    Correction in footer styles

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -7,9 +7,7 @@ function App() {
 
   return (
     <>
-     <h1>
       <Home />
-     </h1>
     </>
   )
 }
--- netflix-clone/src/components/Footer/Footer.jsx
+++ netflix-clone/src/components/Footer/Footer.jsx
@@ -44,12 +44,12 @@ function Footer() {
               </ul>
             </div>
           </div>
-            <div className={FooterCSS["service_code"]}>
+          <div className={FooterCSS["service_code"]}>
               <p>Service Code</p>
-            </div>
-            <div className={FooterCSS["copy_write"]}>
+          </div>
+          <div className={FooterCSS["copy_write"]}>
               &copy; 1997-2024 Netflix, Inc.
-            </div>
+          </div>
         </div>
       </div>
     </>
