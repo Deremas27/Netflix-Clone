920128132ed6078704161d9bb6b6175fc2794066
Dereje Masresha <dbazmm3@gmail.com>
2025-02-10 13:18:44 +0300

    banner is done

--- netflix-clone/src/page/Home/Home.jsx
+++ netflix-clone/src/page/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
 
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
       {/* <h3>{JSON.stringify(import.meta.env)}</h3> */}
     </>
--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,4 +1,5 @@
-const API_KEY = process.env.VITE_REACT_API_KEY;
+// const API_KEY = process.env.VITE_REACT_API_KEY;
+const API_KEY = import.meta.env.VITE_REACT_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&languages=en-US`,
